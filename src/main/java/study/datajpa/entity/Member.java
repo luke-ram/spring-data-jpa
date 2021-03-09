3f06aeed39bd9777f652b133498d2f0d4e2ea701
luke-ram <sooramho@gmail.com>
2021-03-09 09:02:35 +0900

    model 생성 완료

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,26 +1,42 @@
 package study.datajpa.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "userName", "age"})
 public class Member {
-
-    @Id
-    @GeneratedValue
-    private Long id;
-    private String userName;
-
-    protected Member() {
-    }
 
     public Member(String userName) {
         this.userName = userName;
+    }
+
+    public Member(String userName, int age, Team team) {
+        this.userName = userName;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String userName;
+    private int age;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMember().add(this);
     }
 }
---
+++ src/main/java/study/datajpa/entity/Team.java
@@ -0,0 +1,30 @@
+package study.datajpa.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"})
+public class Team {
+
+    public Team(String name) {
+        this.name = name;
+    }
+
+    @Id
+    @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> member = new ArrayList<>();
+
+}

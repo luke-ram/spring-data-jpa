0d86ab10d2982f5d3f058a927179d2d65116c7be
luke-ram <sooramho@gmail.com>
2021-03-10 08:26:59 +0900

    반환타입 완료

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
@@ -15,4 +16,10 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select m from Member m where m.userName in :names")
     List<Member> findByNames(@Param("names") List<String> names);
+
+    List<Member> findListByUserName(String username);
+
+    Member findMemberByUserName(String username);
+
+    Optional<Member> findOptionalByUserName(String username);
 }
--- src/test/java/study/datajpa/repository/MemberRepository.java
+++ src/test/java/study/datajpa/repository/MemberRepository.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.entity.Member;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 @SpringBootTest
@@ -32,11 +33,31 @@ class MemberRepositoryTest {
     @Test
     public void findByNames() {
         Member member1 = new Member("memberA");
-        Member member2 = new Member("memberA");
+        Member member2 = new Member("memberB");
         memberRepository.save(member1);
         memberRepository.save(member2);
 
         memberRepository.findByNames(Arrays.asList("memberA", "memberB"));
+
+
+    }
+
+    @Test
+    public void returnType() {
+
+        Member member1 = new Member("memberA");
+        Member member2 = new Member("memberB");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> memberA = memberRepository.findListByUserName("memberA");
+        System.out.println(memberA.get(0).getUserName());
+
+        Optional<Member> memberA1 = memberRepository.findOptionalByUserName("memberA");
+        System.out.println("memberA1.get().getUserName() = " + memberA1.get().getUserName());
+
+        Member memberA2 = memberRepository.findMemberByUserName("memberA");
+        System.out.println(memberA2.getUserName());
 
 
     }

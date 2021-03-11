ac08c20708b3c46509fc91c251219ff1caccfcbc
luke-ram <sooramho@gmail.com>
2021-03-11 09:19:05 +0900

    web확장 완료

---
+++ src/main/java/study/datajpa/controller/MemberController.java
@@ -0,0 +1,47 @@
+package study.datajpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.entity.Member;
+import study.datajpa.entity.MemberDto;
+import study.datajpa.repository.MemberRepository;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id) {
+        Member member = memberRepository.findById(id).get();
+        return member.getUserName();
+    }
+
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member) {
+        return member.getUserName();
+    }
+
+    @GetMapping("/members")
+    public Page<MemberDto> list(@PageableDefault(size = 100) Pageable pageable) {
+        return memberRepository.findAll(pageable)
+                .map(member -> new MemberDto(member.getId(), member.getUserName(), member.getAge()));
+    }
+
+    @PostConstruct
+    public void init() {
+
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("user" + i));
+        }
+    }
+
+}
